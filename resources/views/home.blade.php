0a105cdce5c5c06fef053ff116be977c19e21ec0
Vahidone <v.ban.roma@gmail.com>
2023-11-20 22:17:21 +0100

    feat: add image to cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
 <div class="container d-flex flex-wrap my-5 py5">
     @foreach ($movies as $movie)
         <div class="card m-2" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="{{ $movie->image}}" class="card-img-top" alt="{{ $movie->title}}">
             <div class="card-body">
             <h5 class="card-title">{{$movie->title}}</h5>
             <p>{{$movie->original_title}}</p>
